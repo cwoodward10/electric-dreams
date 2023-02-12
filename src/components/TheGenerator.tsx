c775ce2a936ef9b1ea9d48c0830020401f28b6a5
woodyframework <c.woodward10@gmail.com>
2023-02-12 11:00:38 -0600

    update with api key and rest api

--- src/components/TheGenerator.tsx
+++ src/components/TheGenerator.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { runGenerator } from "../modules/utility/generator";
+import { runGenerator, GenerationResponse } from '../modules/utility/generator';
 
 function TheGenerator() {
     const [dreamPrompt, setDreamPrompt] = useState('');
@@ -9,52 +9,64 @@ function TheGenerator() {
     /**
      * Array containing {seed, image} objects
      */
-    const [generatedImages, setGeneratedImages] = useState([]);
+    const [generatedImages, setGeneratedImages] = useState(undefined as undefined|GenerationResponse);
 
-    function callbackOnDream(params: any) {
-        setGeneratedImages(generatedImages.concat(params));
-    }
+    let imageElements = generatedImages ? 
+        (
+            <ul>
+                {
+                    generatedImages.artifacts.map((g, index) => {
+                        return (
+                            <li key={index}>
+                                <img src={`data:image/png;base64,${g.base64}`} />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        ) : null;
 
-    function handleOnSubmit(e: any) {
+    async function handleOnSubmit(e: any) {
         e.preventDefault();
 
-        // clear old images
-        setGeneratedImages([]);
-
         // create new images
-        runGenerator(dreamPrompt, callbackOnDream, apiKey, params)
+        const images = await runGenerator(dreamPrompt, apiKey, params);
+        setGeneratedImages(images);
     }
 
     return (
-        <div className="flex flex-col gap-4">
-            <h1>Dare to dream!</h1>
-            <form 
-                onSubmit={(e) => handleOnSubmit(e)}
-                className="flex flex-col gap-2"
-            >
-                <textarea 
-                    name="dream-prompt"
-                    className="w-full rounded p-2 resize-none"
-                    placeholder={'What are you dreaming of...'} 
-                    value={dreamPrompt}
-                    onChange={e => setDreamPrompt(e.target.value)}
-                />
-                <input
-                    name="api-key"
-                    className="w-full rounded p-2"
-                    placeholder={'Provide an API Key'}
-                    value={apiKey}
-                    onChange={e => setApiKey(e.target.value)}
-                />
-                <div className="flex justify-start">
-                    <button 
-                        type='submit'
-                        className="bg-transparent border border-red-400 border-solid inline-block"
-                    >
-                        Dream!
-                    </button>
-                </div>
-            </form>
+        <div>
+            <section className="flex flex-col gap-4">
+                <h1>Dare to dream!</h1>
+                <form 
+                    onSubmit={(e) => handleOnSubmit(e)}
+                    className="flex flex-col gap-2"
+                >
+                    <textarea 
+                        name="dream-prompt"
+                        className="w-full rounded p-2 resize-none"
+                        placeholder={'What are you dreaming of...'} 
+                        value={dreamPrompt}
+                        onChange={e => setDreamPrompt(e.target.value)}
+                    />
+                    <input
+                        name="api-key"
+                        className="w-full rounded p-2"
+                        placeholder={'Provide an API Key'}
+                        value={apiKey}
+                        onChange={e => setApiKey(e.target.value)}
+                    />
+                    <div className="flex justify-start">
+                        <button 
+                            type='submit'
+                            className="bg-transparent border border-red-400 border-solid inline-block"
+                        >
+                            Dream!
+                        </button>
+                    </div>
+                </form>
+            </section>
+            { imageElements }
         </div>
     )
 }
--- src/modules/utility/generator.ts
+++ src/modules/utility/generator.ts
@@ -1,124 +1,53 @@
-import { GenerationServiceClient } from '@stabilitySDK/generation_pb_service';
-import {
-    DiffusionSampler, 
-    ImageParameters, 
-    TransformType,
-    Request,
-    ArtifactType,
-    ClassifierParameters,
-    SamplerParameters,
-    StepParameter,
-    ScheduleParameters,
-    Prompt,
-    FinishReason,
-} from '@stabilitySDK/generation_pb';
-import { grpc } from "@improbable-eng/grpc-web";
+export interface GenerationResponse {
+	artifacts: Array<{
+		base64: string;
+		seed: number;
+		finishReason: string;
+	}>
+}
 
-/**
- * Runs the Stability AI base code along with the provided callback function.
- * The callback function is called if/when images are returned. 
- * @param {*} prompt The prompt by which images should be generated on
- * @param {*} callbackFunc Func called if/when images are returned by the api
- * @param {*} apiKey API key against which to bill for the images
- * @param {*} params Additional params for the generaor
- */
-export function runGenerator(
+export async function runGenerator(
     prompt: string, 
-    callbackFunc: (args: any) => void, 
     apiKey: string, 
     params: any) {
-    // Set up image parameters
-    const imageParams = new ImageParameters();
-    imageParams.setWidth(params.width || 512);
-    imageParams.setHeight(params.height || 512);
-    imageParams.addSeed(params.seed || 1234);
-    imageParams.setSamples(params.samples || 1);
-    imageParams.setSteps(params.steps || 50);
-
-    // Use the `k-dpmpp-2` sampler
-    const transformType = new TransformType();
-    transformType.setDiffusion(DiffusionSampler.SAMPLER_K_DPMPP_2M);
-    imageParams.setTransform(transformType);
-
-    // Use Stable Diffusion 2.0
-    const request = new Request();
-    request.setEngineId(params.engineId || "stable-diffusion-512-v2-1");
-    request.setRequestedType(ArtifactType.ARTIFACT_IMAGE);
-    request.setClassifier(new ClassifierParameters());
-
-    // Use a CFG scale of `13`
-    const samplerParams = new SamplerParameters();
-    samplerParams.setCfgScale(params.cfgScale || 13);
-
-    const stepParams = new StepParameter();
-    const scheduleParameters = new ScheduleParameters();
-
-    // Set the schedule to `0`, this changes when doing an initial image generation
-    stepParams.setScaledStep(0);
-    stepParams.setSampler(samplerParams);
-    stepParams.setSchedule(scheduleParameters);
-
-    imageParams.addParameters(stepParams);
-    request.setImage(imageParams);
-
-    // Set our text prompt
-    const promptText = new Prompt();
-    promptText.setText(prompt);
-
-    request.addPrompt(promptText);
-
-    // Authenticate using your API key, don't commit your key to a public repository!
-    const metadata = new grpc.Metadata();
-    metadata.set("Authorization", "Bearer " + apiKey);
-
-    // Create a generation client
-    const generationClient = new GenerationServiceClient(
-        "https://grpc.stability.ai",
-        {}
-    );
-
-    // Send the request using the `metadata` with our key from earlier
-    const generation = generationClient.generate(request, metadata);
-
-    // Set up a callback to handle data being returned
-    generation.on("data", (data) => {
-        data.getArtifactsList().forEach((artifact) => {
-            // Oh no! We were filtered by the NSFW classifier!
-            if (
-                artifact.getType() === ArtifactType.ARTIFACT_TEXT &&
-                artifact.getFinishReason() === FinishReason.FILTER
-            ) {
-                return console.error("Your image was filtered by the NSFW classifier.");
+        const engineId = 'stable-diffusion-512-v2-0';
+        const apiHost = 'https://api.stability.ai'
+        
+        if (!apiKey) {
+            console.error("Please provide an API key.");
+            return;
+        }
+        
+        const response = await fetch(
+            `${apiHost}/v1beta/generation/${engineId}/text-to-image`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${apiKey}`,
+                },
+                body: JSON.stringify({
+                    text_prompts: [
+                        {
+                            text: prompt,
+                        }
+                    ],
+                    cfg_scale: params.cfgScale ?? 7,
+                    clip_guidance_preset: params.clipGuidance ?? 'FAST_BLUE',
+                    height: params.height ?? 512,
+                    width: params.width ?? 512,
+                    samples: params.samples ?? 1,
+                    steps: params.steps ?? 10,
+                })
             }
-
-            // Make sure we have an image
-            if (artifact.getType() !== ArtifactType.ARTIFACT_IMAGE) return;
-
-            // You can convert the raw binary into a base64 string
-            const base64Image = btoa(
-            new Uint8Array(artifact.getBinary() as Uint8Array).reduce(
-                (data, byte) => data + String.fromCodePoint(byte),
-                ""
-            ));
-
-            // Here's how you get the seed back if you set it to `0` (random)
-            const seed = artifact.getSeed();
-
-            // We're done!
-            callbackFunc(
-                { 
-                    'seed': seed, 
-                    'image': base64Image 
-                }
-            );
-        });
-    });
-
-    // Anything other than `status.code === 0` is an error
-    generation.on("status", (status) => {
-        if (status.code === 0) return;
-        console.error(
-            "Your image could not be generated. You might not have enough credits."
         );
-    });
+        
+        if (!response.ok) {
+            console.error(`Non-200 response: ${await response.text()}`);
+            return;
+        }
+        
+        const responseJSON = await response.json() as GenerationResponse;
+        return responseJSON;
 }
